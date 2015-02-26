85f878acc60cd5d01c8c77f74ba9a01f9a197e48
Guilherme GM <321.guilherme@gmail.com>
2015-02-26 18:13:20 -0300

    crawler db connection

--- crawler/main.py
+++ crawler/main.py
@@ -8,18 +8,20 @@ from pymongo import MongoClient
 
 
 def main():
+    mongo_client = MongoClient()
+    mongo_db = mongo_client.eventos
+    mongo_eventos = mongo_db.eventos
 
     def registra_eventos(eventos):
         for evento in eventos:
             if not mongo_eventos.find({'nome': re.compile(evento['nome'], re.IGNORECASE)}).count():
                 print mongo_eventos.insert(evento)
 
+        print mongo_eventos.find()
+
     saia_de_casa = EventoHandler('https://saiadecasa.github.io/')
     software_livre = SoftwareLivre('http://softwarelivre.org/portal/eventos', ('nome', 'data', 'site', 'local'))
 
-    mongo_client = MongoClient()
-    mongo_db = mongo_client.test
-    mongo_eventos = mongo_db.eventos
 
     registra_eventos(software_livre.get_eventos())
     registra_eventos(saia_de_casa.get_eventos())
